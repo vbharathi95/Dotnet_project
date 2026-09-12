984ea2b3e3c399b0c5d5ea36eb002b10d2a3a697
Bharathi Jagadeesh <vbharathi02032026@gmail.com>
2026-09-12 16:48:09 +0530

    initial commit

---
+++ UrbanCompany/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UrbanCompany.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
